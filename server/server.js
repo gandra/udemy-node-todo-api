92399ad3088dbea566f93327b818191c6029abe0
Dragan Mijatovic <dragan.mijatovic@cramick-it.com>
2017-10-19 18:30:01 +0200

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -18,7 +18,6 @@ app.post('/todos', (req, res) => {
             res.send(doc);
         },
         (e) => {
-            console.error('Unable to save Todo', e);
             res.status(400).send(e);
         },
     );
@@ -27,3 +26,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+module.exports = {app};
