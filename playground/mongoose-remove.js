8c114fb331d7c6e105fa1e13573352cacfba95bd
Dragan Mijatovic <dragan.mijatovic@cramick-it.com>
2017-10-25 13:41:55 +0200

    Test the DELETE /todos/:id route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -3,44 +3,19 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
-// const id = '59e8d70ddfd9981709228e21xx';
-//
-// if (!ObjectID.isValid(id)) {
-//     return console.error(`ID not valid: ${id}`);
-// }
-
-
-// Todo.find({
-//     _id: id
-// }).then((todos) => {
-//     console.log('Todos', todos);
-// });
-//
-// Todo.findOne({
-//     _id: id
-// }).then((todo) => {
-//     console.log('Todo', todo);
-// });
-//
-// Todo.findById(id).then((todo) => {
-//     if (!todo) {
-//         return console.log('Id not found');
-//     }
-//     console.log('Todo by id', todo);
-// }).catch((e) => {
-//     console.error(e);
+// Remove all
+// Todo.remove({}).then((res) => {
+//     console.log(res);
 // });
 
+// Todo.findOneAndRemove();
+// Todo.findByIdAndRemove();
+// Todo.findOneAndRemove();
 
-const id = '59e8c5f93cec091309e6dd8d';
-User
-    .findById(id)
-    .then((user) => {
-        if (!user) {
-            return console.log(`Not found user for id ${id}`);
-        }
-        console.log(JSON.stringify(user, undefined, 2));
-    })
-    .catch((err) => {
-        console.error('Error finding user!', err);
-    });
+Todo.findByIdAndRemove('59f06608632fbf9469a43b95').then((todo) => {
+    console.log(todo);
+});
+
+Todo.findOneAndRemove({_id: '59f06608632fbf9469a43b95'}).then((todo) => {
+    console.log(todo);
+});
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
 
 let app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -53,8 +54,26 @@ app.get('/todos/:id', (req, res) => {
         });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Id not valid');
+    }
+
+    Todo.findByIdAndRemove(id)
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send({});
+            }
+            res.send({todo});
+        })
+        .catch((e) => {
+            res.status(400).send({});
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Started on port $\{port}`);
 });
 
 module.exports = {app};
