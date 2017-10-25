fd90c2ac92cd975b325dbe1cda5047914f9e4c5e
Dragan Mijatovic <dragan.mijatovic@cramick-it.com>
2017-10-25 14:19:00 +0200

    AddPATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -67,13 +68,39 @@ app.delete('/todos/:id', (req, res) => {
             }
             res.send({todo});
         })
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Id not valid');
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send({todo});
+        })
         .catch((e) => {
-            res.status(400).send({});
+            res.status(400).send();
         });
 });
 
 app.listen(port, () => {
-    console.log(`Started on port $\{port}`);
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
