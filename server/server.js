f63101fbced6903f52efae8f16e4d72b83f70dd8
Dragan Mijatovic <dragan.mijatovic@cramick-it.com>
2017-10-25 16:51:01 +0200

    Add token to POST /users route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
+
 const {mongoose} = require('./db/mongoose');
 const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
@@ -103,12 +104,16 @@ app.post('/users', (req, res) => {
     // let body = _.pick(req.body, ['email', 'password', 'tokens']);
     let body = _.pick(req.body, ['email', 'password']);
     let user = new User(body);
-    user.save().then((doc) => {
-        res.send(doc);
-    })
-    .catch((e) => {
-        res.status(400).send(e);
-    });
+    user.save()
+        .then(() => {
+            return user.generateAuthToken();
+        })
+        .then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+        .catch((e) => {
+            res.status(400).send(e);
+        });
 });
 
 app.listen(port, () => {
