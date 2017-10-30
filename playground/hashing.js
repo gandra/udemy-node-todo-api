50714ca6c3cb270447b3ccae269f321b05356581
Dragan Mijatovic <dragan.mijatovic@cramick-it.com>
2017-10-30 16:58:40 +0100

    Has user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+let password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
 
-let data = {
-    id: 10
-};
+let hashedPassword = '$2a$10$5QPUrf32WUS0jPzhzdx5Me0eAL77hY.Y..YD5teh5ywBl6ZtpNvg2';
 
-let token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
 
-let decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// let data = {
+//     id: 10
+// };
+//
+// let token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// let decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 // let message = 'I am a user number 3';
