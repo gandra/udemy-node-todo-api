b1dfdc7f79f64916d62df746899b4aeea1ae4bca
Dragan Mijatovic <dragan.mijatovic@cramick-it.com>
2017-10-31 08:39:52 +0100

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -132,6 +132,14 @@ app.post('/users/login', (req, res) => {
     });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        return res.status(200).send();
+    }, () => {
+        return res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
